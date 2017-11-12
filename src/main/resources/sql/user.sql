67a7adcd263ff5fbdc506a77b20f4799be43c388
GongDiXin <847135055@qq.com>
2017-11-13 00:06:02 +0800

    修改user.sql

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -2,3 +2,4 @@ INSERT INTO `user` VALUES (10001, 'gongdixin', '1992-10-3', '1', '四川', '1234
 INSERT INTO `user` VALUES (10002, 'zhaoliming', '2017-9-6', '0', '内蒙', '123456');
 INSERT INTO `user` VALUES (10003, 'yinmocheng', '2017-9-21', '0', '湖南', '123456');
 INSERT INTO `user` VALUES (10004, 'tianjun', '2017-9-19', '0', '云南', '123456');
+INSERT INTO `user` VALUES (10005, 'feiyu', '2012-10-25', '0', '四川', '123456');
